c7c5708fc5cd3a915d4d52cfe1fd9880b7041908
Nathan Self <nwself@cs.vt.edu>
2019-06-20 11:12:01 -0400

    Hide features on anomaly only row

--- home/home/static/js/anomaly.js
+++ home/home/static/js/anomaly.js
@@ -1,12 +1,27 @@
 $(document).ready(function () {
-  buildTable(anomalyData, '#anomaly_table', true);
+  buildTable({
+    data: anomalyData,
+    selector: '#anomaly_table',
+    hide: true,
+    datatablesSettings: {
+      info: false,
+      ordering: false,
+      paging: false,
+      searching: false,
+    }
+  });
+  // buildTable(anomalyData, '#anomaly_table', true);
 
   // send ajax query to fill in table
   $.ajax({
     url: anomalyUrl, 
     method: 'GET',
     success: function (data) {
-      buildTable(data);
+      // buildTable(data);
+      buildTable({
+        data: data,
+        hide: true
+      })
     },
     error: function (error) {
       console.error(error);

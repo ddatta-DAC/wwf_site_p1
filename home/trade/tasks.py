8735571fd3c0fd75f702b590036debd65a16583d
Nathan Self <nwself@cs.vt.edu>
2019-06-21 15:34:17 -0400

    Rebuild cached csv for each new comment

---
+++ home/trade/tasks.py
@@ -0,0 +1,13 @@
+import os
+
+from django.conf import settings
+from django.core import management
+
+from huey.contrib.djhuey import db_task
+
+
+@db_task()
+def rebuild_csv(track_name):
+    scores_file = os.path.join(settings.BASE_DIR, 'trade', 'data', '{}_scores.csv'.format(track_name))
+
+    management.call_command('build_csv', track_name, scores_file)

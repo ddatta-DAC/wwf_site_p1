752515ff8da2e4ef29916b823e031028b5806d15
Nathan Self <nwself@cs.vt.edu>
2019-10-20 22:04:37 -0400

    Add version number to task csv file lookup

--- home/trade/tasks.py
+++ home/trade/tasks.py
@@ -8,6 +8,6 @@ from huey.contrib.djhuey import db_task
 
 @db_task()
 def rebuild_csv(track_name):
-    scores_file = os.path.join(settings.BASE_DIR, 'trade', 'data', '{}_scores.csv'.format(track_name))
+    scores_file = os.path.join(settings.BASE_DIR, 'trade', 'data', 'v2', '{}_scores.csv'.format(track_name))
 
     management.call_command('build_csv', track_name, scores_file)

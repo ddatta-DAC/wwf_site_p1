9d4e80e164aad569d721d877d15f815944d869bf
Nathan Self <nwself@cs.vt.edu>
2020-03-04 16:11:39 -0500

    Don't add hits for admin sites

--- home/hitcounter/middleware.py
+++ home/hitcounter/middleware.py
@@ -9,6 +9,7 @@ class HitCounterMiddleware:
         return self.get_response(request)
 
     def process_view(self, request, view_func, view_args, view_kwargs):
-        if request.user.is_authenticated:
+        if request.user.is_authenticated and 'admin' not in view_func.__module__:
+            print("save hit")
             save_hit(request.user.pk, "{}:{}".format(view_func.__module__, view_func.__name__))
         return None

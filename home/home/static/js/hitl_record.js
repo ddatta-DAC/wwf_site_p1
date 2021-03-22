b658f77bbe4ef556ece6104c5a1be0c385af59ea
ddatta <ddatta@vt.edu>
2021-03-22 19:21:15 -0400

    Experimental changes

--- home/home/static/js/hitl_record.js
+++ home/home/static/js/hitl_record.js
@@ -5,3 +5,16 @@ $(document).ready(function () {
     order: [[ 2, "desc" ]]
   });
 });
+
+
+window.onscroll = function() {myFunction()};
+var navbar = document.getElementById("viznavbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
